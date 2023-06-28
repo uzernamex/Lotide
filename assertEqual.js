006328ed3c4f815ba81f59633137f61cd67f2e03
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-06-27 23:35:45 -0400

    code added to compare values to one another

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,14 @@
 const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("Assertion Failed: `${actual} !== `${expected}");
+  }
+}
+  
+module.exports = assertEqual;
 
-};
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 2);
+assertEqual(1, 1);
