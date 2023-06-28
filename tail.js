988c595910f0a29ef5230a95172a3f957837e3de
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-06-28 18:14:50 -0400

    assertEqual function added

---
+++ tail.js
@@ -0,0 +1,13 @@
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
+*/
+
+const tail = function(array){
+return array.slice[0]
+}
+
