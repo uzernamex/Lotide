2d09ee3a7f626644b5968629428d654128d7b7ef
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-06-28 17:26:20 -0400

    added test cases

--- head.js
+++ head.js
@@ -9,7 +9,11 @@ const assertEqual = function(actual, expected) {
 */
 
 const head = function(array){
-  return array[0]
+
+  return array.slice(-1);
 }
+
+console.log(head([4, 8, 2, 6]));
+
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
