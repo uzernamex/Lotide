58378d867f9dd554f889fb58ace531fdcfe02443
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-06-28 20:14:49 -0400

    more test cases added for assurance

--- tail.js
+++ tail.js
@@ -8,20 +8,18 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(array) {
   const result = array.slice(1);
-  return result 
+  return result
 };
 
-//assertEqual(console.log(tail([5, 4, 2, 7, 2])));
-//assertEqual (console.log(tail([1, 2, 3, 4, 5, 6, 7, 8, 9])));
-// 
-
-
-//const result1 = tail([7, 2, 3, 4, 8])
-tail 
-// Test Case: Check the original array 
+// Test Case 1: Check the original array 
 const result1 = ["Yo Yo", "Lighthouse", "Labs"];
 tail(result1); // no need to capture the return value since we are not checking it
 assertEqual(result1.length, 3); // original array should still have 3 elements!
+
+// Test Case 2: Check the original array
+const result2 = [333, 808, 416, 905, 6]
+tail(result2); 
+assertEqual(result2.length, 5); 
 
 // Test Case 1: Check the returned array elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
@@ -29,3 +27,10 @@ assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
+// Test Case 2: Check the returned array elements
+const result3 = tail([18, 99, 1992, 42, 24]);
+assertEqual(result3.length, 4); 
+assertEqual(result3[0], 99); 
+assertEqual(result3[1], 1992); 
+assertEqual(result3[2], 42);
+assertEqual(result3[3], 24);
