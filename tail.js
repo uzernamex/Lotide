ffefdb4ffacb203d4d6c45819a6ff93644fcef8c
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-06-28 20:52:57 -0400

    linting completed with eslint

--- tail.js
+++ tail.js
@@ -7,25 +7,24 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
- for (let i = 0; i < array.length; i++) {
-  const result = array.slice(1);
-  return result
- }
- return []
+  for (let i = 0; i < array.length; i++) {
+    const result = array.slice(1);
+    return result;
+  }
 };
 
-// Test Case 1: Check the original array 
+// Test Case 1: Check the original array
 const result1 = ["Yo Yo", "Lighthouse", "Labs"];
 tail(result1); // no need to capture the return value since we are not checking it
 assertEqual(result1.length, 3); // original array should still have 3 elements!
 
 // Test Case 2: Check the original array
-const result2 = [333, 808, 416, 905, 6]
-tail(result2); 
-assertEqual(result2.length, 5); 
+const result2 = [333, 808, 416, 905, 6];
+tail(result2);
+assertEqual(result2.length, 5);
 
-const result4 = [4]
-tail(result4)
+const result4 = [4];
+tail(result4);
 
 assertEqual(result4.length, 1);
 
@@ -37,11 +36,11 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 // Test Case 2: Check the returned array elements
 const result3 = tail([18, 99, 1992, 42, 24]);
-assertEqual(result3.length, 4); 
-assertEqual(result3[0], 99); 
-assertEqual(result3[1], 1992); 
+assertEqual(result3.length, 4);
+assertEqual(result3[0], 99);
+assertEqual(result3[1], 1992);
 assertEqual(result3[2], 42);
 assertEqual(result3[3], 24);
 
-const result5 = tail([8]); 
+const result5 = tail([8]);
 assertEqual(result5.length, 0);
