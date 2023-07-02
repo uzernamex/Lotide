5de0d0bc1294c4bb459582afb2bd05f9a713f5d3
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 18:32:25 -0400

    refactored code and removed unnecessary lines

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,11 @@
 const assertArraysEqual = function(arr1, arr2) { 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length !== arr2.length) {
-      console.log("[❌🤬❌] Assertion Failed: The arrays are not a match💔");
-    }
-    if (arr1[i] === arr2[i]) {
-      console.log("[✅⭐️💚] Assertion Passed: The arrays are a match!💚");
+    if (arr1[i] !== arr2[i]) {
+      console.log("[❌🤬❌] The arrays are not a match💔");
+      return;
     }
   }
+    console.log("[✅⭐️💚] The arrays are a match!💚")
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
