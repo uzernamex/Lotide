3e72543cb6e1fce3e1161c7993b3fbecb22296d5
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 17:49:24 -0400

    added more test conditions for eqArrays and assertEqual

--- eqArrays.js
+++ eqArrays.js
@@ -1,29 +1,30 @@
-/*const assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
   } else {
     console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
   }
 };
-*/
+
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]){
+    if (arr1[i] !== arr2[i]) {
       return false;
       }
-    
-  } if (arr1[i] === arr2[i]) {
-    return true; }
+    }
+    return true; 
 };
 
 
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([0, 1, 3], ["0", "one", 33]), false);
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
