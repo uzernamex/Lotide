c5b6edaba96311ba4ba2634322461c0639e3e435
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 17:41:01 -0400

    moved truthy and falsy values

--- eqArrays.js
+++ eqArrays.js
@@ -7,17 +7,16 @@
 };
 */
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]){
-        return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]){
+      return false;
       }
-    }
-  } else {
-    return false;
-  }
+    
+  } if (arr1[i] === arr2[i]) {
+    return true; }
 };
 
 
