2e1f8753ff6ec07bb0a8d6a0dd02e2627beac768
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 17:33:33 -0400

    added test cases to function

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
+*/
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]){
+        return true;
+      }
+    }
+  } else {
+    return false;
+  }
+};
+
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
