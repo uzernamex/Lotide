700b093036b6eb70d112aa48537fc9b443b9e76a
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 18:00:30 -0400

    assertEqual function added to test cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,10 @@
-
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
