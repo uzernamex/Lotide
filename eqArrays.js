7ba78a9e7d72afa4c16c51ed79e466b15fb0ecca
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 17:52:27 -0400

    lint testing completed

--- eqArrays.js
+++ eqArrays.js
@@ -6,23 +6,22 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-      }
     }
-    return true; 
+  }
+  return true;
 };
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 assertEqual(eqArrays([0, 1, 3], ["0", "one", 33]), false);
+
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
