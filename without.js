8db05f99d4f0c01e9a5878b388f25cab289319ac
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 20:21:36 -0400

    code linted

--- without.js
+++ without.js
@@ -1,12 +1,11 @@
 const without = function(source, itemsToRemove) {
   const result = [];
   for (let i of source) {
-  if (itemsToRemove.includes(i)) {
+    if (itemsToRemove.includes(i)) {
       result.push(i);
     }
   }
-  
-return result;
+  return result;
 };
 
 console.log(without([1, 2, 3], [1])); 
