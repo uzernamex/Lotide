5ff4e9bb478065aed43044717c556231518d3b9b
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 20:20:30 -0400

    test code

--- without.js
+++ without.js
@@ -1,11 +1,12 @@
 const without = function(source, itemsToRemove) {
   const result = [];
-  for (let i of source)
-  if (itemsToRemove[i]) {
-      result.push[i];
+  for (let i of source) {
+  if (itemsToRemove.includes(i)) {
+      result.push(i);
     }
+  }
   
 return result;
-}
+};
 
 console.log(without([1, 2, 3], [1])); 
