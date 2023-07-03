978ce66b207134c56c3c130850a481c051e022c3
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 21:09:32 -0400

    linted code for middle assignment

--- middle.js
+++ middle.js
@@ -22,11 +22,11 @@ const eqArrays = function(arr1, arr2) {
 const middle = function(arr) {
   if (arr.length < 2) {
     return [];
-}
-const middlemost = Math.floor(arr.length / 2)
-if (arr.length % 2 === 0) {
-  return [arr[middlemost -1], arr[middlemost]];
-} else {
-  return [arr[middlemost]];
-}
+  }
+  const middlemost = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) {
+    return [arr[middlemost -1], arr[middlemost]];
+  } else {
+    return [arr[middlemost]];
+  }
 };
