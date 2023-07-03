0fe26b5685399009fd81920e975883971c3b56bb
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 20:50:01 -0400

    assertArraysEqual and eqArrays functions added to code

---
+++ middle.js
@@ -0,0 +1,20 @@
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+*/
