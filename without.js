f7e9fa68f149cf70d34fcaaa93f86b568ab3b035
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-02 20:16:59 -0400

    .push added to code

--- without.js
+++ without.js
@@ -2,8 +2,10 @@ const without = function(source, itemsToRemove) {
   const result = [];
   for (let i of source)
   if (itemsToRemove[i]) {
-      
+      result.push[i];
     }
   
 return result;
 }
+
+console.log(without([1, 2, 3], [1])); 
