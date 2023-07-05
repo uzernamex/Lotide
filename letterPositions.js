025c5710d4fa5937efac297c47f8e5cb8af0ce3e
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 19:33:57 -0400

    code for testing functions eqArrays and assertEqual added

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,31 @@
 //We'll implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([0, 1, 3], ["0", "one", 33]), false);
+  
+
 const letterPositions = function() {
   const results = {};
   return results;
-}
+};
+
