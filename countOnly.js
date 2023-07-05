7110df521cf8abd98d84ade06dfa07963f7ca191
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 15:45:32 -0400

    updated for loop to increment value

--- countOnly.js
+++ countOnly.js
@@ -19,6 +19,19 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
+
+
+// inside the loop, 
+// increment the counter for each item:
+//   set a property with that string key to:
+//     the value that was already there (or zero if nothing there) with 1 added to it.
+if (results[item]) {
+  results[item] += 1;
+} else {
+  results[item] = 1;
+}
+
+
     console.log(item);
   }
   return results;
