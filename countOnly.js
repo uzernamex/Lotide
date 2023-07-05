bf093a36cbcf408424f788bc909b4b4c16545233
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 09:07:26 -0400

    starter code added to count only file

---
+++ countOnly.js
@@ -0,0 +1,23 @@
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
+
+*/
+
+// countOnly will be given an array and an object. 
+//It will return an object containing counts of everything that the input object listed.
+//As shown in the example image above, only keys which have a truthy value should be counted in the resulting object. 
+//All other strings (either set to false or not included at all in the object) should not be counted. 
+//if a particular string is meant to be counted but does not exist in the input array does not have to be included in the final count.
+//The function should report back how many instances of each string were found in the allItems array of strings.
+
+
+
+const countOnly = function(allItems, itemsToCount) {
+
+}
+
