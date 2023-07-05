9e931598e3b15ac5641df1d548cca80a046387e1
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 19:12:13 -0400

    comments and instrictions removed

--- countLetters.js
+++ countLetters.js
@@ -6,36 +6,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
-
-//We need it report back multiple numbers.
-
-//return an object where each unique character in the string is the KEY 
-//&& the number of occurrences of that character is the VALUE of that key.
-//we can skip and not count spaces, as seen here.
-//can use for...of loops with strings.
-
-//item we are isolating gets returned in same line / console.loh
-// results gets own if statement + that is where you assign the key:value pairs
-//do this inside the for of loop
-//template is that of a function as an if-else statement 
-
 const countLetters = function(string) {
   const results = {};
-
   for (const stringLetter of string) {
-
-if (stringLetter !== " ") {
-
-
-    if (results[stringLetter]) {
-      results[stringLetter] += 1;
-    } else {
-      results[stringLetter] = 1;
+    if (stringLetter !== " ") {
+      if (results[stringLetter]) {
+        results[stringLetter] += 1;
+      } else {
+        results[stringLetter] = 1;
+      }
     }
   }
-  }
-  
   return results;
 };
 
