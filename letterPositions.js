cb5bdc4794c8630d597648c1463cfe06a0ac56af
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 19:37:43 -0400

    assertArraysEqual fuction added to code

--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,15 @@
 //We'll implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
-  } else {
-    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+const assertArraysEqual = function(arr1, arr2) { 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log("[❌🤬❌] The arrays are not a match💔");
+      return;
+    }
   }
+    console.log("[✅⭐️💚] The arrays are a match!💚")
 };
 
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -20,8 +23,8 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([0, 1, 3], ["0", "one", 33]), false);
+assertEqual(eqArrays(["hello"], {[1, 2, 3]}), true); 
+
   
 
 const letterPositions = function() {
