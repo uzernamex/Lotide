944c86e713d52a910b79c54044462bef79c2162f
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 19:29:57 -0400

    function implementation

---
+++ letterPositions.js
@@ -0,0 +1,5 @@
+//We'll implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+const letterPositions = function() {
+  const results = {};
+  return results;
+}
