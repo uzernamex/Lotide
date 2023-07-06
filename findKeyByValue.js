2cdce9bbdd104d0c33edf76f5d8b0d3b9da006b5
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 21:35:58 -0400

    changed method of comparison

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,9 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//Implement the function findKeyByValue which takes in an object and a value. 
-//It should scan the object and return the first key which contains the given value. 
-//If no key with that given value is found, then it should return undefined.
 
 const findKeyByValue = function(object, value) {
   const result = [];
