c5a3a69b1ed180d4cf903869deec334da58c4cce
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 21:21:21 -0400

    function definition and result added to code

---
+++ eqObjects.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Else: false
+
+const eqObjects = function(object1, object2) {
+const result = ""
+return result;
+};
+
+console.log(eqObjects({one: 1, two: 2, three: 3}), ({one: 1, two: 2, three: 3}));
