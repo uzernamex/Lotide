974e0b7a7d07ad5e7ae15df9cad875a35c2ffed2
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 21:32:47 -0400

    declared variables to represent the keys in the object

--- eqObjects.js
+++ eqObjects.js
@@ -11,7 +11,22 @@ const assertEqual = function(actual, expected) {
 
 const eqObjects = function(object1, object2) {
 const result = ""
+for (let key1 of object1) {
+  for (let key2 of object2)
+  if (key1 === key2) {
+    return true
+  } else {
+    return false
+  }
+}
 return result;
 };
 
-console.log(eqObjects({one: 1, two: 2, three: 3}), ({one: 1, two: 2, three: 3}));
+console.log(assertEqual(eqObjects({one: 1, two: 2, three: 3}), ({one: 1, two: 2, three: 3})));
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+console.log(assertEqual(eqObjects(shirtObject , anotherShirtObject))); // => true
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+console.log(assertEqual(eqObjects(shirtObject , longSleeveShirtObject))); // => false
