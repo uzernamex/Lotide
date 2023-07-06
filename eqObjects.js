367b5c52df790228846720037227f1f657cad50f
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 21:59:33 -0400

    added function 'eqArrays' to ensure array equality can be tested

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,14 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
