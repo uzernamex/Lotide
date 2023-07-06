e7650a67821dd99dfb16d13a4f40fb26d9a1e00f
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 20:53:29 -0400

    assertion code added to new file

---
+++ findKeyByValue.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
+  } else {
+    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
+  }
+};
+  
