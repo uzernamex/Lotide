fdce42ecac3b4c19c6145e5210ad5dd25e6a0aa8
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-05 21:02:06 -0400

    starter code first attempt

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,4 +5,20 @@ const assertEqual = function(actual, expected) {
     console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
   }
 };
-  
+
+//Implement the function findKeyByValue which takes in an object and a value. 
+//It should scan the object and return the first key which contains the given value. 
+//If no key with that given value is found, then it should return undefined.
+
+const findKeyByValue = function(object, value) {
+  const result = object[]
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+
+    } else {
+      return undefined
+    }
+  } 
+  return result;
+};
