08ab627f5aaf77dd774a7d3645541b7c14a25212
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-07-08 17:27:33 -0400

    updated code for eq objects and assertion added

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,7 +1,26 @@
 
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  let control = true;
+  for (let key in object1) {
+    for (let key2 in object2) {
+      if (key === key2) {
+        if (object1[key] === object2[key2]) {
+          control = true;
+        } else {
+          control = false;
+        }
+      }
+    }
+  }
+  return control;
+};
+
+/*  if (keys1.length !== keys2.length) {
+    return false;
+  }
   
   for (let key of keys1) {
     const objectOne = object1[key];
@@ -15,6 +34,21 @@ const eqObjects = function(object1, object2) {
         return false;
       }
     }
-  } 
+  }
   return true;
 };
+*/
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`Assertion failed: ${inspect(actual)} !== ${inspect(expected) }`);
+  }
+};
+
+const first = { a: '1', b: 2 };
+const second = { b: 2, a: '1' };
+
+assertObjectsEqual(first, second);
