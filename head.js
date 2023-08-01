2152d3fdd26eedcddfd0a13d55ac5595b44f47c8
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 17:50:03 -0400

    test code transferred to new file

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
-  } else {
-    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const head = function(array){
   let i = array[0];
-  return i
-}
+  return i;
+};
 
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
