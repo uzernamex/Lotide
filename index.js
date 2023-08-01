0bde6bd391ccfd13df4eb472d972f61b030dae99
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 19:40:20 -0400

    added functions to index file

---
+++ index.js
@@ -0,0 +1,33 @@
+// index.js
+
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeybyValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPositions  = require('./letterPositions');
+const middle = require('./middle');
+const tail = require('./tail');
+const without = require('./without');
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual, 
+  assertObjectsEqual: assertObjectsEqual, 
+  countLetters: countLetters, 
+  countOnly: countOnly, 
+  eqArrays: eqArrays, 
+  eqObjects: eqObjects, 
+  findKeyByValue: findKeyByValue,
+  flatten: flatten, 
+  head: head,
+  letterPositions: letterPositions, 
+  middle: middle, 
+  tail: tail, 
+  without: without,
+};
