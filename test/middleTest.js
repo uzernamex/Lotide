1ddb327fe878159fe7c85221622523777b212fd2
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 21:37:37 -0400

    test cases added

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,1 +1,11 @@
-const middleTest = ('../require')
+const assert = require('chai').assert;
+const middleTest = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+});
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(middle(['5']), '5'); 
+});
