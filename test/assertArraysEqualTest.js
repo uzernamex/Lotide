e046f58bc600a94a0cb67213005ef12e8adf5434
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 21:39:51 -0400

    added test case

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,21 @@
 const assertEqual = require('../assertEqual');
-const assertArraysEqual = ('../require assertArraysEqual');
+const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, "3"]);
 assertArraysEqual([12, 2, 3], [1, 2, 3]);
+
+
+const assert = require('chai').assert;
+const assertArraysEqualTest = require('../assertArraysEqual');
+
+describe("#assertArraysEqual"), () => {
+  it("returns true for ([1, 2, 3], [1, 2, 3])"); () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  };
+
+
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3])); 
+})
+};
