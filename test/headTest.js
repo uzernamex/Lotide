49082917b450da5a071a9880ca17ba0dcf5b891d
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 21:38:17 -0400

    test cases added to file

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,12 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+it("returns 'hello' for ['hello', 'lighthouse' 'labs']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
