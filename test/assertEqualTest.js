cb1ab9cb600072f7b85ab24eab344b2ebe49ab7e
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 21:39:16 -0400

    test cases added

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assertEqualTest = require('../assertEqual');
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+const assert = require('chai').assert;
+const assertArraysEqualTest = require('../middle');
+
+describe("#assertEqual", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(assertEqual([1, 2, 3]), 2);
+  });
+});
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(assertEqual(['5']), '5'); 
+});
