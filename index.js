561f93f29d3a26ae123721aedc6f5265a58d776f
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 21:33:54 -0400

    formatting

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
+const findKey = require('.findKey');
 const findKeyByValue = require('./findKeybyValue');
 const flatten = require('./flatten');
 const head = require('./head');
-const letterPositions  = require('./letterPositions');
+const letterPositions = require('./letterPositions');
 const map = require('./map');
 const middle = require('./middle');
 const tail = require('./tail');
 const without = require('./without');
+
 
 module.exports = {
   assertArraysEqual,
