80ea3d04e2aa582be553d199e948ab1d6ae4f488
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 21:38:46 -0400

    test cases added

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,18 @@
 const assertEqual = require('../assertEqual');
-const eqArraysTest = require ('../eqArrays');
 
+const assert = require('chai').assert;
+const eqArraysTest = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+describe("#eqArrays", () => {
+  it("returns === for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3]), [1, 2, 3/]);
+  });
+});
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(eqArrays(['5']), '5'); 
+});
+
+/* assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 assertEqual(eqArrays([0, 1, 3], ["0", "one", 33]), false);
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
@@ -10,3 +20,4 @@ console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+*/
