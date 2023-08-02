70a5e76d93ea1fe6c8b7a83fe62eaf2f26c0eb8d
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 21:41:45 -0400

    linting completed

--- middle.js
+++ middle.js
@@ -1,34 +1,13 @@
-/*const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
-  } else {
-    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-*/
-
 const middle = function(arr) {
   if (arr.length < 2) {
     return [];
   }
   const middlemost = Math.floor(arr.length / 2);
   if (arr.length % 2 === 0) {
-    return [arr[middlemost -1], arr[middlemost]];
+    return [arr[middlemost - 1], arr[middlemost]];
   } else {
     return [arr[middlemost]];
   }
 };
 
-module.exports = middle; 
+module.exports = middle;
