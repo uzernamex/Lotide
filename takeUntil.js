2ab2f058940f47242cf6ebb31ccd799ac23a643b
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-01 20:46:10 -0400

    refactored function

--- takeUntil.js
+++ takeUntil.js
@@ -1,9 +1,11 @@
 const takeUntil = function(array, callback) {
   const result = [];
-}
-for (let item of array) {
-  if (callback(item)) {
-    result.push(item);
-  } 
-} else { return result;
+  for (let item of array) {
+    if (!callback(item)) {
+      result.push(item);
+    } else {
+      return result;
+    }
+  }
+  return result
 };
