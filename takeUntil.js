855fa88f9474565ba7ecc85ce47086ab9c719951
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-04 19:59:04 -0400

    code linting complete

--- takeUntil.js
+++ takeUntil.js
@@ -1,11 +1,17 @@
+//This function returns a new array with the elements until the specified element in the callback is present.
 const takeUntil = function(array, callback) {
+  // init empty array.
   const result = [];
   for (let item of array) {
+    // uses a for loop to iterate through the given array.
     if (!callback(item)) {
       result.push(item);
+      //if the condition is not met then add to result.
     } else {
-      return result;
+      return result; //otherwise, do not add to result, just return the included elements.
     }
   }
-  return result
+  return result;
 };
+
+module.exports = takeUntil;
