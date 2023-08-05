2514b97f972ed4a37618a47343aaaf443da6c6ca
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-04 20:17:54 -0400

    checking for lint

--- letterPositions.js
+++ letterPositions.js
@@ -1,8 +1,9 @@
+//Function returns the location of a character in a given sentence / string.
 const letterPositions = function(sentence) {
-  const results = {};
+  const results = {};//init result as object.
   for (let i = 0; i < sentence.length; i++) {
-    const character = sentence[i];
-    if ((character !== " ") && (results[character])) {
+    const character = sentence[i]; //iterate through each element of sentence
+    if ((character !== " ") && (results[character])) {//if not a space and is present in results object.
       results[character].push(i);
     } else {
       results[character] = [i];
