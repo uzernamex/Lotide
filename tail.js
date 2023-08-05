5febfc4257a40c2d1d2df46e9ca9cf7d6fa06841
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-04 20:05:14 -0400

    eslint complete

--- tail.js
+++ tail.js
@@ -1,16 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
-  } else {
-    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
-  }
-};
-
+//This function takes in an array and returns a copy of that array with the first element omitted.
 const tail = function(array) {
   for (let i = 0; i < array.length; i++) {
-    const result = array.slice(1);
+    const result = array.slice(1); //return the elements in the array starting at position '1' instead of '0'.
     return result;
   }
 };
 
-module.exports = tail; 
+module.exports = tail;
