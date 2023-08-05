f55f8d982833011dd4e1cc24052e181c5516fbba
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-04 20:34:38 -0400

    checked linting before submission

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("[✅⭐️💚] Assertion Passed: `${actual}` === `${expected}");
-  } else {
-    console.log("[❌🤬❌] Assertion Failed: `${actual} !== `${expected}");
-  }
-};
-
-
 const countOnly = function(allItems, itemsToCount) {
-  const results = {};
+  const results = {}; //init result as an empty object
   for (const item of allItems) {
     if (itemsToCount[item]) {
-      if (results[item]) {
+      if (results[item]) { //if the results contain the item then add to count
         results[item] += 1;
       } else {
         results[item] = 1;
@@ -21,24 +12,5 @@ const countOnly = function(allItems, itemsToCount) {
   }
   return results;
 };
-  
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
