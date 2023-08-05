a14b8d2b62d1b9e13772f6ed3c80040435201c5f
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-04 20:09:52 -0400

    code refactored

--- middle.js
+++ middle.js
@@ -1,12 +1,13 @@
+//This code returns the middlemose element of a given array.
 const middle = function(arr) {
   if (arr.length < 2) {
-    return [];
+    return []; // returns and empty array if there are less than 2 elements.
   }
-  const middlemost = Math.floor(arr.length / 2);
-  if (arr.length % 2 === 0) {
-    return [arr[middlemost - 1], arr[middlemost]];
+  const middlemost = Math.floor(arr.length / 2); //splits the array in half to reach mid-point.
+  if (arr.length % 2 === 0) { // if even amount of elements in array..
+    return [arr[middlemost - 1], arr[middlemost]];// return the two items in the middle.
   } else {
-    return [arr[middlemost]];
+    return [arr[middlemost]];// if odd, return just the middle element in the array.
   }
 };
 
