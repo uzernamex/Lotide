6b18dabe9010459d05d7d68c5225904c86ed7688
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-04 20:27:41 -0400

    linted code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,8 +1,8 @@
 const findKeyByValue = function(object, value) {
   const result = [];
-  for (let key in object) {
+  for (let key in object) { //iterate through each key in the object using for-in loop
     if (object[key] === value) {
-      return value;
+      return value; //if the key is the same as the value then return as the result.
     } else {
       return undefined;
     }
